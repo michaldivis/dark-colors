09b43b144681c108e1618fdaf3919e8ef0c2f7d6
Michal Diviš <michall.divis@gmail.com>
2022-12-21 16:18:48 +0100

    bumped version to 1.1.0, added DominantColorCandidate XML comments

--- src/DarkColors/DominantColorCandidate.cs
+++ src/DarkColors/DominantColorCandidate.cs
@@ -4,6 +4,14 @@ using System.Linq;
 
 namespace DarkColors;
 
+/// <summary>
+/// A potential dominant color candidate
+/// </summary>
+/// <param name="Color">The color</param>
+/// <param name="Brightness">The brightness the a color</param>
+/// <param name="Saturation">The saturation of the color</param>
+/// <param name="SpaceCoverage">The amount of space in the image the color covers</param>
+/// <param name="NonGreyscaleScore">The greater the non-greyscale score, the further the color is from a gray-ish tone... the more colorful it is</param>
 public record DominantColorCandidate(Color Color, float Brightness, float Saturation, float SpaceCoverage, float NonGreyscaleScore)
 {
     internal static DominantColorCandidate Create(IEnumerable<Color> colors, int count, int totalCount)
